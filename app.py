3a32476585de47cec4a177d24c42dd326511fe48
sstephen345 <sstephen345@gmail.com>
2026-07-04 17:21:58 +0100

    Add first Streamlit app

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Trading Dashboard",
+    page_icon="📈",
+    layout="centered"
+)
+
+st.title("📈 Trading Dashboard")
+
+st.success("Welcome Stephen!")
+
+st.write("This is Version 0.1")
+
+st.write("🚧 Under Development")
